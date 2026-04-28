f7d0799e556a0225d94bee1a729fda229966924e
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-04-28 22:40:06 +0530

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        int[] freq = new int[101]; // since nums[i] ≤ 100
+        int count = 0;
+        
+        for (int num : nums) {
+            count += freq[num]; // pairs formed with previous same numbers
+            freq[num]++;
+        }
+        
+        return count;
+    }
+}
