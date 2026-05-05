bf905a1111a19327a45d64ac4ca90ce7a3a37466
Vachhani Heet Rajubhai <vachhaniheet1504@gmail.com>
2026-05-05 20:50:06 +0530

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        
+        for (String s : details) {
+            // Extract age (2 characters)
+            int age = Integer.parseInt(s.substring(11, 13));
+
+            if (age > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
